c9cdb7df0782ccbd046e207063571ab70fdd97b5
ygoyal2003 <107212826+ygoyal2003@users.noreply.github.com>
2024-09-13 11:14:23 +0530

    Add files via upload

---
+++ Movie Rental Prediction.py
@@ -0,0 +1,94 @@
+# Start your coding from below
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# For lasso
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Run OLS
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Random forest
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import RandomizedSearchCV
+
+# Read in data
+df_rental = pd.read_csv("archive (2)/rental_info.csv")
+
+# Add information on rental duration
+df_rental["rental_length"] = pd.to_datetime(df_rental["return_date"]) - pd.to_datetime(df_rental["rental_date"])
+df_rental["rental_length_days"] = df_rental["rental_length"].dt.days
+
+### Add dummy variables
+# Add dummy for deleted scenes
+df_rental["deleted_scenes"] =  np.where(df_rental["special_features"].str.contains("Deleted Scenes"), 1, 0)
+# Add dummy for behind the scenes
+df_rental["behind_the_scenes"] =  np.where(df_rental["special_features"].str.contains("Behind the Scenes"), 1, 0)
+
+# Choose columns to drop
+cols_to_drop = ["special_features", "rental_length", "rental_length_days", "rental_date", "return_date"]
+
+# Split into feature and target sets
+X = df_rental.drop(cols_to_drop, axis=1)
+y = df_rental["rental_length_days"]
+
+# Further split into training and test data
+X_train,X_test,y_train,y_test = train_test_split(X, 
+                                                 y, 
+                                                 test_size=0.2, 
+                                                 random_state=9)
+
+# Create the Lasso model
+lasso = Lasso(alpha=0.3, random_state=9) 
+
+# Train the model and access the coefficients
+lasso.fit(X_train, y_train)
+lasso_coef = lasso.coef_
+
+# Perform feature selectino by choosing columns with positive coefficients
+X_lasso_train, X_lasso_test = X_train.iloc[:, lasso_coef > 0], X_test.iloc[:, lasso_coef > 0]
+
+# Run OLS models on lasso chosen regression
+ols = LinearRegression()
+ols = ols.fit(X_lasso_train, y_train)
+y_test_pred = ols.predict(X_lasso_test)
+mse_lin_reg_lasso = mean_squared_error(y_test, y_test_pred)
+
+# Random forest hyperparameter space
+param_dist = {'n_estimators': np.arange(1,101,1),
+          'max_depth':np.arange(1,11,1)}
+
+# Create a random forest regressor
+rf = RandomForestRegressor()
+
+# Use random search to find the best hyperparameters
+rand_search = RandomizedSearchCV(rf, 
+                                 param_distributions=param_dist, 
+                                 cv=5, 
+                                 random_state=9)
+
+# Fit the random search object to the data
+rand_search.fit(X_train, y_train)
+
+# Create a variable for the best hyper param
+hyper_params = rand_search.best_params_
+
+# Run the random forest on the chosen hyper parameters
+rf = RandomForestRegressor(n_estimators=hyper_params["n_estimators"], 
+                           max_depth=hyper_params["max_depth"], 
+                           random_state=9)
+rf.fit(X_train,y_train)
+rf_pred = rf.predict(X_test)
+mse_random_forest= mean_squared_error(y_test, rf_pred)
+
+# Random forest gives lowest MSE so:
+best_model = rf
+best_mse = mse_random_forest
+
+print(best_mse)
